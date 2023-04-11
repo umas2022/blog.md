2caf0f6862cfe0e5196b916054f6ca3b305fd0fa
umas <1970313791@qq.com>
2023-04-11 06:26:43 +0200

    hello electron

--- 1_example/electron/hello_world/vue.config.js
+++ 1_example/electron/hello_world/vue.config.js
@@ -1,42 +1,3 @@
-# hello electron
-electron项目基本操作
-
-## hello world
-- 使用vue+electron创建一个简单的单窗口页面
-- 项目位置: 1_example/electron
-
-### 1. 一个简单的窗口
-- 参见 hello_vue.md 创建一个基于vue的 hello world 项目
-- 在项目中添加electron支持
-```
-vue add electron-builder
-    (Electron Version ^13.0.0)
-```
-- 在package.json中可以看到新增的命令
-```
-npm run electron:serve
-npm run electron:build
-```
-- 直接使用```npm run electron:serve```启动测试时background.ts可能会报错Object is of type 'unknown',需要手动给报错的变量定义类型:any
-```
-catch (e) 改为 catch (e:any)
-```
-- 还有可能报错TypeError: loaderContext.getOptions is not a function,需要降低ts-loader的版本到8.2.0
-```
-npm install ts-loader@~8.2.0
-```
-- 还有可能在src/router/index.ts中报错File 'xxx/src/views/HomeView.vue.ts' is not a module.,这是因为HomeView.vue缺少了export,简单的修改方法是添加setup参数
-```html
-<script setup lang="ts">
-或者
-<script lang="ts">
-export default {};
-</script>
-```
-
-### 2. 常用的一些打包选项
-- vue.config.js
-```js
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -95,31 +56,3 @@ module.exports = defineConfig({
     }
   }
 });
-
-```
-
-
-### 3. 窗口的一些附加参数
-
-```js
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    frame: false,        // 隐藏默认窗口边框
-    title:"hello_title", // 标题属性不能生效,真正的标题在public/index.html中
-    resizable: false,    // 指定窗口是否可以被用户调整大小
-    minimizable: false,  // 指定窗口是否可以被最小化或最大化
-    maximizable: false, 
-    fullscreen: true,    // 指定窗口是否全屏显示
-    backgroundColor: "red", // 背景颜色
-    transparent: true, // 指定窗口是否为透明,如果设置为 true,则可以使用 setOpacity 方法来设置窗口的透明度
-  })
-
-  // 设置窗口透明度,需要transparent: true
-  win.setOpacity(0.5)
-```
-
-
-
-
-
